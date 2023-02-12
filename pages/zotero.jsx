2e4830ec0108ff7ea01097567b6730c4287c68ee
sehandev <sehandev@gmail.com>
2023-02-12 08:35:23 +0000

    Fix

--- pages/zotero.jsx
+++ pages/zotero.jsx
@@ -4,7 +4,9 @@ import { useRouter } from "next/router"
 function Page() {
   const router = useRouter()
   const { zotero_url } = router.query
-  const zotero_id = zotero_url.split('/items/')[1]
+
+  if (zotero_url) {
+    const zotero_id = zotero_url.split("/items/")[1]
 
     const meta_content = `0; URL=zotero://select/library/items/${zotero_id}`
     return (
@@ -12,6 +14,8 @@ function Page() {
         {zotero_id && <meta http-equiv="refresh" content={meta_content} />}
       </Head>
     )
+  }
+
   return <></>
 }
 
