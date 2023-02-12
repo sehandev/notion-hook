b28183a0c8b837daa6248b11964999fde9f07c2f
sehandev <sehandev@gmail.com>
2023-02-12 08:34:04 +0000

    Edit url

--- pages/zotero.jsx
+++ pages/zotero.jsx
@@ -3,7 +3,8 @@ import { useRouter } from "next/router"
 
 function Page() {
   const router = useRouter()
-  const { zotero_id } = router.query
+  const { zotero_url } = router.query
+  const zotero_id = zotero_url.split('/items/')[1]
 
     const meta_content = `0; URL=zotero://select/library/items/${zotero_id}`
     return (
